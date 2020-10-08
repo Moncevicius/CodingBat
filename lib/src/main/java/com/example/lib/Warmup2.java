aa44ae086ec989ce1d0734a07a30c039a815e43a
Mantas Moncevicius <moncevicius@googlemail.com>
2020-10-08 04:16:57 +0100

    update

--- lib/src/main/java/com/example/lib/Warmup2.java
+++ lib/src/main/java/com/example/lib/Warmup2.java
@@ -401,7 +401,14 @@ public class Warmup2 {
 
     public boolean has271(int[] nums){
 
-        
+        for(int i=0; i < nums.length-2; i++){
+            if(nums[i] + 5 == nums[i+1]){
+                if(Math.abs((nums[i]-1) - nums[i+2]) <= 2){
+                    return true;
+                }
+            }
+        }
+        return false;
 
     }
 
